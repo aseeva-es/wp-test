c0bdb1c83a11f1527927e3f693002aea3ec5e868
aseeva.e.s <aseeva.e.s@gmail.com>
2025-01-20 12:08:31 +0200

    dotenv functionality added

--- app.php
+++ app.php
@@ -1,4 +1,16 @@
 <?php 
 require_once __DIR__ . '/vendor/autoload.php';
-$hello=new Elena\Wptest\Hello();
-echo $hello->sayHello();
+use App\Weather;
+use Dotenv\Dotenv;
+
+$dotenv= Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+
+
+$weather = new Weather();
+$currentWeather = $weather->getCurrentWeather($_ENV['API_URL']);
+
+echo "Current weather in Helsinki:\n";
+echo "Temperature: {$currentWeather['temperature']}°C\n";
+echo "Humidity: {$currentWeather['humidity']}%\n";
